d49b8a15c9bba7a64a2f15e6ee4d0df698e9d42f
Benjamin Lupa <147424978+BenjaminLupa@users.noreply.github.com>
2024-01-24 11:25:51 +0100

    feat: solved task color-mixer

--- part-1-vue-basics/color-mixer-App-with-vue/src/script.js
+++ part-1-vue-basics/color-mixer-App-with-vue/src/script.js
@@ -8,38 +8,73 @@
 4) das rgb als Hexadezimal ausgeben
 */
 
-const red = document.querySelector("#red"); //1
-const green = document.querySelector("#green"); //1
-const blue = document.querySelector("#blue"); //1
-const slider = document.querySelector("header"); //1
-const rgbNum = document.querySelector("#rgbNum"); //1
-const hexaNum = document.querySelector("#hexaNum");
+// const red = document.querySelector("#red"); //1
+// const green = document.querySelector("#green"); //1
+// const blue = document.querySelector("#blue"); //1
+// const slider = document.querySelector("header"); //1
+// const rgbNum = document.querySelector("#rgbNum"); //1
+// const hexaNum = document.querySelector("#hexaNum");
 
-slider.addEventListener("input", function () {
-  //2.1
-  changeColor("--redValue", red.value); //2.2
-  changeColor("--greenValue", green.value); //2.2
-  changeColor("--blueValue", blue.value); //2.2;
-  rgbNum.innerText =
-    "rgb(" + red.value + ", " + green.value + ", " + blue.value + ")";
-  convertRgb();
-});
-function changeColor(customColor, colorValue) {
-  //2.3
-  document.documentElement.style.setProperty(customColor, colorValue);
-}
+// slider.addEventListener("input", function () {
+//   //2.1
+//   changeColor("--redValue", red.value); //2.2
+//   changeColor("--greenValue", green.value); //2.2
+//   changeColor("--blueValue", blue.value); //2.2;
+//   rgbNum.innerText =
+//     "rgb(" + red.value + ", " + green.value + ", " + blue.value + ")";
+//   convertRgb();
+// });
+// function changeColor(customColor, colorValue) {
+//   //2.3
+//   document.documentElement.style.setProperty(customColor, colorValue);
+// }
 
-function convertRgb() {
-  //4
-  let redVal = red.value;
-  let greenVal = green.value;
-  let blueVal = blue.value;
-  let hexColorCode =
-    "#" + getHexa(redVal) + getHexa(greenVal) + getHexa(blueVal);
-  hexaNum.innerText = hexColorCode;
-}
-function getHexa(value) {
-  //4
-  let hexa = parseInt(value).toString(16);
-  return hexa.length == 1 ? "0" + hexa : hexa;
-}
+// function convertRgb() {
+//   //4
+//   let redVal = red.value;
+//   let greenVal = green.value;
+//   let blueVal = blue.value;
+//   let hexColorCode =
+//     "#" + getHexa(redVal) + getHexa(greenVal) + getHexa(blueVal);
+//   hexaNum.innerText = hexColorCode;
+// }
+// function getHexa(value) {
+//   //4
+//   let hexa = parseInt(value).toString(16);
+//   return hexa.length == 1 ? "0" + hexa : hexa;
+// }
+
+const app = Vue.createApp({
+  data() {
+    return {
+      redValue: "255",
+      greenValue: "0",
+      blueValue: "255",
+      hexaNumber: "#ff69b4",
+    };
+  },
+  computed: {},
+  methods: {
+    changeColor(color, colorVal) {
+      document.documentElement.style.setProperty(`--${color}`, colorVal);
+      this.convertRgb();
+    },
+    convertRgb() {
+      //4
+      let redVal = this.redValue;
+      let greenVal = this.greenValue;
+      let blueVal = this.blueValue;
+      let hexColorCode =
+        "#" +
+        this.getHexa(redVal) +
+        this.getHexa(greenVal) +
+        this.getHexa(blueVal);
+      this.hexaNumber = hexColorCode;
+    },
+    getHexa(value) {
+      //4
+      let hexa = parseInt(value).toString(16);
+      return hexa.length == 1 ? "0" + hexa : hexa;
+    },
+  },
+}).mount("#app");
